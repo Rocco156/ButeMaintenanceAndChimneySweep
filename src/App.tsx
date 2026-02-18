2ff9d41137a426779e66009ec7f51d53983c9fe7
Rocco156 <mrkingdice74@gmail.com>
2026-02-18 17:30:22 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,8 @@ function App() {
   return (
     <main>
       <h1>Bute Maintenance & Chimney Sweep</h1>
-      /*<button onClick={createTodo}>+ new</button> */
+	  <!--
+      <button onClick={createTodo}>+ new</button> -->
       <ul>
         <li>joinery</li>
 		<li>sweeping</li>
