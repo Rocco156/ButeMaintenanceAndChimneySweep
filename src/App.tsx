2d26e57dd14a26240563b4203683cdb7a7e65b55
Rocco156 <mrkingdice74@gmail.com>
2026-02-18 16:39:11 +0000

    added new logo & changed page data

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,8 @@ function App() {
 
   return (
     <main>
-      <h1>hello world</h1>
-      <button onClick={createTodo}>+ new</button>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
-        ))}
-      </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+      <h1>Bute Maintenance & Chimney Sweep</h1>
+      
     </main>
   );
 }
