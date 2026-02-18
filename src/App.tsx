66be1073976e996fc00bf9f15c7168f915e46886
Rocco156 <mrkingdice74@gmail.com>
2026-02-18 18:53:00 +0000

    changed image size & added text

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App() {
 
   return (
     <main>
-	  <img src="/transparent-logo.svg" alt="Large logo" style="width:500px;height:500px;"/>    
+	  <img src="/transparent-logo.svg" alt="Large logo"/>    
 	  <br />
       <h1>Bute Maintenance & Chimney Sweep</h1>
 	  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent facilisis mi eget tortor blandit, a aliquet lacus bibendum. Duis tempor nisi ac neque sollicitudin, vitae gravida orci consequat. Nunc pulvinar quis tellus sit amet volutpat. Quisque faucibus purus ex, facilisis suscipit magna convallis fermentum. Cras et magna et purus tempor viverra sit amet a turpis. Praesent urna magna, tempor quis est quis, interdum volutpat quam. Mauris id scelerisque lectus. Etiam sit amet tempus metus. Quisque venenatis mi vitae nulla egestas feugiat. Nam vehicula orci turpis, fermentum tincidunt mi condimentum quis. Curabitur tincidunt libero nec ante ornare tempus. </p>
